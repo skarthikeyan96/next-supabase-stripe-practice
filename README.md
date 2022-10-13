f02168398d8dfc9f1ee46a9c967f9f8dacb7ec1f
Karthikeyan Shanmugam <karthikeyan@bangthetable.com>
2022-10-14 00:12:33 +0530

    feat: add login with github and downgrade the version of supabase

--- README.md
+++ README.md
@@ -12,3 +12,16 @@ NEXT_PUBLIC_SUPABASE_URL=
 NEXT_PUBLIC_SUPABASE_KEY=
 
 ```
+
+## GITHUB AUTHENTICATION USING SUPABASE
+
+1. Create a Oauth application by heading over to this [url](https://github.com/settings/applications/new).
+2. Fill in the necessary details and create application.
+3. Now , navigate to `auth/settings` in supabase dashboard and disable `email authentication and email confirmations`.
+4. Enable `Github` and enter in the `Client ID` and `Client Secret` which you got while creating the Oauth application.
+5. 
+
+Note: 
+
+1. homepage url should match the project url from supabase dashboard. 
+2. callback should be `${projectURL}/auth/v1/callback`
--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,22 +1,64 @@
-import Link from "next/link";
 import { useRouter } from "next/router";
-
+import React, { useEffect } from "react";
+import supabaseClient from "../utils/supabase";
 
 interface Layout {
-    children: JSX.Element
+  children: JSX.Element;
 }
 
-
 const Layout = ({ children }: Layout) => {
-    const router = useRouter();
+  const router = useRouter();
+
+  const [user, setUser] = React.useState(null);
+
+  useEffect(() => {
+    checkUser();
+
+    // will be fired when there is a change the part of the URL beginning with and following the # symbol
+    window.addEventListener("hashchange", () => {
+      checkUser();
+    });
+  }, []);
+
+  const checkUser = () => {
+    const user: any = supabaseClient.auth.user();
+    setUser(user);
+  };
+
+  const handleLogin = async () => {
+    await supabaseClient.auth.signIn({
+      provider: "github",
+    });
+  };
+
+  const handleLogout = async () => {
+    await supabaseClient.auth.signOut();
+    router.push("/");
+    setUser(null);
+  };
+
+  const renderUser = () => {
+    const userData: any = user;
+
+    if (userData) {
+      return (
+        <>
+          {" "}
+          <p> {userData.email} </p>{" "}
+          <button onClick={handleLogout}> Logout </button>
+        </>
+      );
+    }
+  };
 
   return (
     <div className="flex flex-col p-8">
       <nav className="w-full max-w-2xl flex mx-auto items-center justify-center">
-
+        {!user && <button onClick={handleLogin}> Login </button>}
       </nav>
 
       <main className="flex flex-col  px-4 mt-16">
+        {renderUser()}
         <div className="flex flex-col items-start max-w-2xl mx-auto pb-16">
           {children}
         </div>
