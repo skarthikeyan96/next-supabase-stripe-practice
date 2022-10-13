608df272364d4f3bac45f2d4f6824ad64cc20e85
Karthikeyan Shanmugam <karthikeyan@bangthetable.com>
2022-10-13 16:18:24 +0530

    feat: querying all data from supabase

---
+++ README.md
@@ -0,0 +1,14 @@
+# SUPABASE SASS DEMO
+
+This is a demo sass application built using next.js , supabase and stripe.
+
+## SETTIING UP YOUR ENV
+
+Create a `.env.local` file in root and paste the following values from the supabase dashboard
+
+```shell
+
+NEXT_PUBLIC_SUPABASE_URL=
+NEXT_PUBLIC_SUPABASE_KEY=
+
+```
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,36 @@
 import type { NextPage } from "next";
 import Layout from "../components/Layout";
+import supabaseClient from "../utils/supabase";
 
-const Home: NextPage = () => {
+
+const Home: NextPage = (props:any) => {
  
+  console.log(props)
   return (
     <Layout>
-     <p> working </p>
+    {
+      props.lessons.map((lesson:any) => {
+        return(
+          <p key={lesson.id}> {lesson.title} </p>
+         
+        )
+      })
+    }
     </Layout>
   );
 };
 
+// pre-render using get static props
+export const getStaticProps = async () => {
+
+  // query all the data from the lesson table
+  const {data} = await supabaseClient.from('Lesson').select('*')
+
+  console.log(data)
+  return {
+    props: {
+      lessons: data
+    }
+  }
+}
 export default Home;
